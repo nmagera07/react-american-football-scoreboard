70a0cfe05fe67fff0e0b62d3781cbc762a84dbe9
Nate Magera <nmagera07@gmail.com>
2019-07-08 18:43:51 -0400

    working on some stretch goals

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, {useState} from "react";
+import React, {useState, useEffect, useRef} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
@@ -7,11 +7,9 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeCount, setHomeCount] = useState(0)
   const [awayCount, setAwayCount] = useState(0)
-  const [count, setCount] = useState(15000)
-
-  const timer = setInterval(function() {
-    setCount(count - 1)
-  }, 10000)
+  
+  
+  
 
 
   return (
@@ -20,15 +18,17 @@ function App() {
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
+            <h2>Home</h2>
+            <h2 className="home__name">Steelers</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
             <div className="home__score">{homeCount}</div>
           </div>
-          <div className="timer">{timer}</div>
+          <div className="timer">{<Timer />}</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
+            <h2>Away</h2>
+            <h2 className="away__name">Browns</h2>
             <div className="away__score">{awayCount}</div>
           </div>
         </div>
@@ -49,5 +49,28 @@ function App() {
     </div>
   );
 }
+
+const Timer = () => {
+    const [time, setTime] = useState(new Date().getTime())
+    const secondsPassed = useRef(900);
+
+    useEffect(() => {
+      const timeout= setTimeout(() => {
+        const date = new Date()
+        secondsPassed.current = secondsPassed.current - 1;
+        setTime(date.getMilliseconds())
+      }, 1000)
+      return () => {
+        clearTimeout(timeout)
+      }
+    }, [time])
+
+    return (
+    <div>
+      <div>{time}</div>
+      <div>{secondsPassed.current}</div>
+    </div>
+  )
+  }
 
 export default App;
